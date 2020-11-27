f74a87b8dc564990a2e55f5b8dc9910ca579b977
Jean Loberg Hansen <jean@lait.dk>
2020-11-27 18:24:51 +0100

    Work on display grid 16

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,52 @@
 <template>
   <div class="grid-w">
-    
     <HeaderComponent />
     <div class="grid-r">
-      <div class="test-div grid-c-2">
+      <div class="test-div grid-c-16">
+        <h2>sidste</h2>
+      </div>
+    </div>
+    <div class="grid-r">
+      <div class="test-div grid-c-4">
+        <h2>item</h2>
+        <div class="grid-r">
+          <div class="grid-c-8 test-div">col</div>
+          <div class="grid-c-8 test-div">col</div>
+        </div>
+      </div>
+      <div class="test-div grid-c-4">
+        <h2>item</h2>
+        <div class="grid-r">
+          <div class="grid-c-4 test-div">col</div>
+          <div class="grid-c-4 test-div">col</div>
+          <div class="grid-c-4 test-div">col</div>
+          <div class="grid-c-4 test-div">col</div>
+        </div>
+      </div>
+      <div class="test-div grid-c-4">
+        <h2>item</h2>
+          <div class="grid-r">
+          <div class="grid-c-16 test-div">col</div>
+        </div>
+      </div>
+      <div class="test-div grid-c-4">
         <h2>item</h2>
       </div>
-      <div class="test-div grid-c-2">
+    </div>
+    <div class="grid-r">
+      <div class="test-div grid-c-4">
         <h2>item</h2>
       </div>
-      <div class="test-div grid-c-3">
+      <div class="test-div grid-c-4">
+        <h2>items</h2>
+             <div class="grid-r">
+          <div class="grid-c-8 test-div">col</div>
+           <div class="grid-c-8 test-div">col</div>
+        </div>
+      </div>
+      <div class="test-div grid-c-4">
         <h2>item</h2>
       </div>
-       <div class="test-div grid-c-4">
-        <h2>item</h2>
-      </div>
-      <div class="test-div grid-c-5">
-        <h2>item</h2>
-      </div>
- 
     </div>
     <Nuxt />
   </div>
